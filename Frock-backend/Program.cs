29e003e8532e75d9fba390ffaccc11ef373b8ea4
adrianvalerio <adrian_2004_valerio@hotmail.com>
2025-06-20 06:40:39 -0500

    feat : add builder for application

--- Frock-backend/Program.cs
+++ Frock-backend/Program.cs
@@ -1,36 +1,158 @@
-ï»¿using Microsoft.EntityFrameworkCore;
-using Frock_backend.access_and_identity.Application.Interfaces;
-using Frock_backend.access_and_identity.Application.Services;
-using Frock_backend.access_and_identity.Domain.Repositories;
-using Frock_backend.access_and_identity.Infrastructure.Persistence;
-using Frock_backend.access_and_identity.Infrastructure.Repositories;
+using Frock_backend.IAM.Application.Internal.CommandServices;
+using Frock_backend.IAM.Application.Internal.OutboundServices;
+using Frock_backend.IAM.Application.Internal.QueryServices;
+using Frock_backend.IAM.Domain.Repositories;
+using Frock_backend.IAM.Domain.Services;
+using Frock_backend.IAM.Infrastructure.Hashing.BCrypt.Services;
+using Frock_backend.IAM.Infrastructure.Persistence.EFC.Repositories;
+using Frock_backend.IAM.Infrastructure.Pipeline.Middleware.Extensions;
+using Frock_backend.IAM.Infrastructure.Tokens.JWT.Configuration;
+using Frock_backend.IAM.Infrastructure.Tokens.JWT.Services;
+using Frock_backend.IAM.Interfaces.ACL;
+using Frock_backend.IAM.Interfaces.ACL.Services;
+using Frock_backend.Profiles.Application.Internal.CommandServices;
+using Frock_backend.Profiles.Application.Internal.QueryServices;
+using Frock_backend.Profiles.Domain.Repositories;
+using Frock_backend.Profiles.Domain.Services;
+using Frock_backend.Profiles.Infrastructure.Persistence.EFC.Repositories;
+using Frock_backend.Shared.Domain.Repositories;
+using Frock_backend.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using Frock_backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Frock_backend.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Add CORS Policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllPolicy",
+        policy => policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+if (connectionString == null) throw new InvalidOperationException("Connection string not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    if (builder.Environment.IsDevelopment())
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+    else if (builder.Environment.IsProduction())
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Error);
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
 
-// Database
-builder.Services.AddDbContext<AccessIdentityDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "ACME.FrockBackend.API",
+            Version = "v1",
+            Description = "ACME Frock Backend API",
+            TermsOfService = new Uri("https://acme-learning.com/tos"),
+            Contact = new OpenApiContact
+            {
+                Name = "ACME Studios",
+                Email = "contact@acme.com"
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Apache 2.0",
+                Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")
+            }
+        });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+    options.EnableAnnotations();
+});
 
-// Dependency Injection
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Profiles Bounded Context Dependency Injection Configuration
+builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
+builder.Services.AddScoped<IProfileCommandService, ProfileCommandService>();
+builder.Services.AddScoped<IProfileQueryService, ProfileQueryService>();
+
+
+// IAM Bounded Context Injection Configuration
+// TokenSettings Configuration
+builder.Services.Configure<TokenSettings>(builder.Configuration.GetSection("TokenSettings"));
+
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserCommandService, UserCommandService>();
+builder.Services.AddScoped<IUserQueryService, UserQueryService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IHashingService, HashingService>();
+builder.Services.AddScoped<IIamContextFacade, IamContextFacade>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
 }
 
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging() || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        c.RoutePrefix = string.Empty; // Opcional: para que Swagger sea la página raíz
+        c.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+
+    });
+
+}
+
+app.UseCors("AllowAllPolicy");
+
+app.UseRequestAuthorization();
+
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
